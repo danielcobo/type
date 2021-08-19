760ea1bee965ac137a9cc7f1b38cadb9cc7e5983
danielcobo <danielcobo@users.noreply.github.com>
2021-08-19 10:45:01 +0200

    Add isFloat()

---
+++ src/isFloat.js
@@ -0,0 +1,10 @@
+/**
+ * Returns true/false if input is a floating number (aka. includes decimals)
+ * @param {*} val - input value
+ * @returns {boolean} true/false input value is a floating number
+ */
+const isFloat = function isFloat(val) {
+  return typeof val === 'number' && !Number.isInteger(val);
+};
+
+module.exports = isFloat;
---
+++ src/isFloat.test.js
@@ -0,0 +1,9 @@
+const isFloat = require('./isFloat.js');
+
+test('Test for float number', function () {
+  expect(isFloat(0.1)).toStrictEqual(true);
+  expect(isFloat(1)).toStrictEqual(false);
+  expect(isFloat(1n)).toStrictEqual(false);
+  expect(isFloat('0.1')).toStrictEqual(false);
+  expect(isFloat(1 / 10)).toStrictEqual(true);
+});
